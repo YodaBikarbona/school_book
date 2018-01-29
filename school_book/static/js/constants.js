7db1eba0b0a1219aa1b1ad8d0156bdbee53901c7
Mihael <mihael@Mihaels-MacBook-Air.local>
2018-01-29 11:30:36 +0100

    frontend services

--- school_book/static/js/constants.js
+++ school_book/static/js/constants.js
@@ -1,4 +1,5 @@
 angular.module('school_book')
+
 .constant('STATES',{ 
 	login:{
 		name:'login',
@@ -16,3 +17,39 @@ angular.module('school_book')
 		}
 	}
 })
+
+.constant('FORM_ERROR',{
+  requiredMessage : 'Ovo polje je obavezno',
+
+  invalidFacultyname : "Naziv Fakulteta može sadržavati slova i brojeve",
+  invalidFacultyminlength : "Naziv fakulteta mora sadžavati bar dva znaka",
+  invalidFacultymaxlength : "Naziv fakulteta ne može biti preko 40 znakova",
+
+  invalidCity : "Naziv grada može sadržavati slova i brojeve",
+  invalidCityminlength: "Naziv grada mora sadžavati bar dva znaka",
+  invalidCitymaxlength: "Naziv grada ne može biti preko 30 znakova",
+
+  invalidAddress : "Adresa može sadržavati slova brojeve i _",
+  invalidAddressminlength : "Adresa mora sadžavati bar dva znaka",
+  invalidAddressmaxlength : "Adresa ne može biti preko 40 znakova",
+
+  invalidPhone : "Telefon može sadržavati samo brojeve",
+  invalidPhoneminlength : "Telefon mora imati barem 6 brojeva",
+  invalidPhonemaxlength : "Telefon ne može imati više od 20 brojeva"
+})
+
+.constant('API_ENDPOINT',{
+  url : 'http://localhost:6543'
+})
+//url : 'http://178.236.87.173:6543/tff_api/v1.0'
+//url : 'http://localhost:6543/tff_api/v1.0'
+
+//.constant('ROLE',{
+ // superadmin : 1,
+ //      admin : 2,
+ //    profesor: 3,
+ //    asistent: 4,
+ //    student : 5
+
+
+//})
---
+++ school_book/static/js/services.js
@@ -0,0 +1,113 @@
+.service('authservice', ['$http','API_ENDPOINT','$q', function($http,API_ENDPOINT,$q){
+      var LOCAL_STORAGE_KEY = 'mytoken'
+      var authToken;
+      var isAuthenticated = false;
+      var role = '';
+      var userObj = {};
+
+      function loadUserCredidentals(){
+        var token = window.localStorage.getItem('mytoken')
+          if(token){
+            isAuthenticated = true;
+             $http.defaults.headers.common['Authorization'] = token;
+          }
+      }
+
+      function storeUserCredidentals(user){
+        window.localStorage.setItem(LOCAL_STORAGE_KEY, user.token);
+        window.localStorage.setItem('user', JSON.stringify(user))
+        useUserCredidentals(user);
+
+      }
+
+      function useUserCredidentals(user){
+        userObj = user;
+        isAuthenticated = true;
+        authToken = user.token;
+        role = user.role.role_name;
+
+        $http.defaults.headers.common['Authorization'] = authToken;
+
+      }
+
+      function destroyUserCredidentals(){
+        isAuthenticated = false;
+        userObj = {};
+        role = '';
+        window.localStorage.clear(LOCAL_STORAGE_KEY);
+        window.localStorage.clear('user');
+         $http.defaults.headers.common.Authorization = undefined;
+        
+
+      }
+
+      var login = function(user){
+        return $q(function(resolve, reject){
+          $http({
+            url: 'http://localhost:6543/login',
+            method: 'POST',
+            data: user
+          }).then(function(resp){
+            console.log(resp)
+            if(resp.data.user){
+              if(resp.data.user.role.role_name == 'admin'){
+                if(resp.data.user.email == username && resp.data.token){
+                  storeUserCredidentals(resp.data);
+                  resolve(resp.data);
+                  $state.go('admin')
+                }
+              }
+            }
+            else{
+              reject(resp.data)
+              console.log('Error')
+            }
+          }, function(resp){
+            console.log(resp)
+          })
+        })
+
+
+        /*return $q(function(resolve,reject){
+                    $http({
+                          url :     API_ENDPOINT.url + '/login',
+                          method : 'POST',
+                          data :    user
+      }).then(function(resp){
+          if(resp.data.status = 'Ok'){
+            storeUserCredidentals(resp.data.user_data);
+              resolve(resp.data);
+          }else{
+            reject(resp.data);
+            
+          }
+        },function(resp){
+          console.log(resp)
+            
+          });
+  })*/}
+        loadUserCredidentals();
+      
+      var logout = function(){
+        destroyUserCredidentals();
+      }
+
+      return {
+        login : login,
+        logout : logout,
+        isAuthorized : true,
+        isAuthenticated : function(){return isAuthenticated;},
+        userObj : function(){return userObj;},
+        role: function(){return role;}
+      }
+  }])
+
+.factory('AuthInterceptor', ['$rootScope', function($rootScope){
+    var inter = {}
+    
+   return inter;
+}])
+
+.config(['$httpProvider',function($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+}])
